fe5b22ef14970f32f90a1b70d6f401df9d297c09
firasmindhi123 <firas.inception@gmail.com>
2023-10-27 21:11:24 +0530

    login

--- controller/user.js
+++ controller/user.js
@@ -1,5 +1,6 @@
 const bcrypt=require('bcrypt')
 const user_model=require('../model/model_user')
+const jwt=require('jsonwebtoken')
 function IsStringInvalid(str)
 {
     if(str==undefined||str.length===0)
@@ -11,6 +12,9 @@ function IsStringInvalid(str)
         return false
     }
 }
+function generateAccesstoken(id,ispremium){
+    return jwt.sign({userid:id},'securatewq')
+  }
 
 exports.signup=async(req,res,next)=>{
 
@@ -50,4 +54,44 @@ exports.signup=async(req,res,next)=>{
         }
         
     
+    }
+    exports.login=async(req,res)=>{
+        try{
+    
+        email=req.body.email
+        password=req.body.password
+        
+        const a=await user_model.findAll({where:{email:`${email}`}})
+        if(IsStringInvalid(email)||IsStringInvalid(password))
+        {
+            return res.status(400).json({success:false,message:"something is missing"})
+        }
+        if(a.length>0)
+        {
+            bcrypt.compare(password,a[0].password,(err,result)=>{
+              if(err)
+              {
+                throw new Error('SOMETHING WENT WRONG')
+              } 
+              if(result==true)
+              {
+                console.log(a)
+                
+    
+                res.status(200).json({success:true,message:"login successfull",token:generateAccesstoken(a[0].id)})
+            }
+            else{
+             res.status(402).json({success:false,message:"password is wrong"})
+            }
+            })
+           
+        }
+        else{
+            
+            res.status(404).json({success:false,message:'email is wrong'})
+           
+        }
+    }catch(err){
+        res.status(500).json({message:err})
+    }
     }
---
+++ public/login/login.js
@@ -0,0 +1,26 @@
+let form=document.getElementById('form')
+form.addEventListener('submit', onsubmit) 
+    async function onsubmit(e)
+{
+ try{
+    e.preventDefault();
+    let email=document.getElementById('email').value
+    let password=document.getElementById('password').value
+    let my_obj={
+        email,
+        password
+ 
+    }
+
+
+ let response =await axios.post("http://localhost:3000/user/login",my_obj)
+  localStorage.setItem('token',response.data.token)
+     window.location.href="../chat/chat.html"
+   
+   
+ }
+ catch(err){
+    console.log(JSON.stringify(err))
+    document.body.innerHTML+=`<div style='color:red;'>${err.message} </div>`
+ }
+}
